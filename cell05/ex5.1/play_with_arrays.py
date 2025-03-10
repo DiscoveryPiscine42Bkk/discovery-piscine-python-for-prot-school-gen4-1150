07e92c59c865d4e53c2c7d3271c96acae196b2a6
gen4-1150 <41284@prot.ac.th>
2025-03-10 21:34:20 +0700

    Add files via upload

---
+++ cell05/ex5.1/play_with_arrays.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def play_with_arrays():
+    """
+    Defines an array, creates a new array by adding 2 to each element,
+    and displays both arrays.
+    """
+
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    new_array = []
+
+    for number in original_array:
+        new_array.append(number + 2)
+
+    print("Original Array:", original_array)
+    print("New Array:", new_array)
+
+if __name__ == "__main__":
+    play_with_arrays()
