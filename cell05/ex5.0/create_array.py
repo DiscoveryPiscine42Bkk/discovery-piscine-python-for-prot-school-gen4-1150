cece1b1e787c0eb20233c71be67e6728fe99e3e6
gen4-1150 <41284@prot.ac.th>
2025-03-10 21:30:24 +0700

    Add files via upload

---
+++ cell05/ex5.0/create_array.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def create_and_display_array():
+    """Creates an array of numbers and displays it."""
+
+    my_array = [10, 25, 30, 45, 50, 65, 70, 85, 90, 100]  # Example array
+    print("My Array:", my_array)
+
+if __name__ == "__main__":
+    create_and_display_array()
