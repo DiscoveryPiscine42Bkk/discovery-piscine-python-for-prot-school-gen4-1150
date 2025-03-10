f1c5df63150b2c6e42a6a79c1ca8f30282a21c25
gen4-1150 <41284@prot.ac.th>
2025-03-10 18:12:49 +0700

    Add files via upload

---
+++ cell03/ex3.0/to25.py
@@ -0,0 +1,16 @@
+def to25():
+    try:
+        num = int(input("Enter a number less than 25\n"))
+
+        if num > 25:
+            print("Error")
+        else:
+            while num <= 25:
+                print(f"Inside the loop, my variable is {num}")
+                num += 1
+
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+    to25()
